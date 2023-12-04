0e11d60625cb583aefadb17093ca450578010125
Pedrobittarbarao <pedrobaraocontas@gmail.com>
2023-12-04 16:15:10 -0300

    projeto

--- main.tf
+++ main.tf
@@ -2,94 +2,42 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = ">= 5.0.0"
     }
   }
-
   required_version = ">= 1.2.0"
+  backend "s3" {
+    bucket         = "bucket-terraform-insper-grupo-e"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    //dynamodb_table = "dynamodb-locks-table-insper"
+  }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0fc5d935ebf8bc3bc"
-  instance_type = "t2.micro"
+  name = "my-vpc"
+  cidr = "172.31.0.0/16"
+  azs = var.azs
+
+  # Define CIDR blocks for your private subnets
+  private_subnets = ["172.31.0.0/26", "172.31.0.64/26"]
+
+  # Define CIDR blocks for your public subnets
+  public_subnets = ["172.31.0.128/26", "172.31.0.192/26"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Terraform = "true"
+    Environment = "Development"
+    Project = "My"
   }
 }
-
-resource "aws_db_subnet_group" "my_db_subnet_group" {
- name      = "my_db_subnet_group"
- subnet_ids = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
-
- tags = {
-   Name = "My DB subnet group"
- }
-}
-
-
-resource "aws_security_group" "ec2_sg" {
- name       = "ec2_sg"
- description = "Allow inbound traffic from EC2 instances"
-
- ingress {
-   from_port  = 22
-   to_port    = 22
-   protocol   = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
- }
-
- egress {
-   from_port  = 0
-   to_port    = 0
-   protocol   = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
- }
-}
-
-resource "aws_security_group" "rds_sg" {
- name       = "rds_sg"
- description = "Allow inbound traffic from EC2 instances"
-
- ingress {
-   from_port  = 3306
-   to_port    = 3306
-   protocol   = "tcp"
-   security_groups = [aws_security_group.ec2_sg.id]
- }
-
- egress {
-   from_port  = 0
-   to_port    = 0
-   protocol   = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
- }
-}
-
-resource "aws_db_instance" "default" {
- allocated_storage   = 20
- storage_type        = "gp2"
- engine              = "mysql"
- engine_version      = "5.7"
- instance_class      = "db.t2.micro"
- db_name                = "mydb"
- username            = "dbuser"
- password            = "dbpassword"
- parameter_group_name = "default.mysql5.7"
- backup_retention_period = 7
- maintenance_window  = "Mon:00:00-Mon:03:00"
- multi_az            = true
- vpc_security_group_ids = [aws_security_group.rds_sg.id]
- db_subnet_group_name = aws_db_subnet_group.my_db_subnet_group.name
- skip_final_snapshot = true
-}
-
-
-
-
-
